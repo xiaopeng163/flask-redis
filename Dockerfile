7e8649af0c2d85a58b6db510eeeb2ec95d5bef26
Peng Xiao <pengxiao@company.com>
2021-07-23 12:17:42 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9.5-slim
+
+RUN pip install flask redis && \
+    groupadd -r flask && useradd -r -g flask flask && \
+    mkdir /src && \
+    chown -R flask:flask /src
+
+USER flask
+
+COPY app.py /src/app.py
+
+WORKDIR /src
+
+ENV FLASK=app.py
+
+EXPOSE 5000
+
+CMD ["flask", "run", "-h", "0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from redis import StrictRedis
+import os
+import socket
+
+app = Flask(__name__)
+redis = StrictRedis(host=os.environ.get('REDIS_HOST', '127.0.0.1'),
+                    port=6379, password=os.environ.get('REDIS_PASS', 'redis'))
+
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return f"Hello Container World! I have been seen {redis.get('hits').decode('utf-8')} times and my hostname is {socket.gethostname()}.\n"
